c53719fede62fba168d2aa6afef13bdc2bbb234a
mnycz <mnycz04@gmail.com>
2020-12-03 20:04:11 -0500

    Created log folder for recording actions

--- logger.py
+++ logger.py
@@ -10,7 +10,7 @@ def log(member):
     :param member: member object
     """
     elap_time: str = str(round((member.leave_time - member.join_time), 0))
-    user_log_title = f"log_{member.name}.txt"
+    user_log_title = f"logs/log_{member.name}.txt"
     user_log = open(user_log_title, 'a')
     user_log.write(elap_time + ',\n')
     user_log.close()
@@ -20,6 +20,6 @@ def log_actions(text):
     """logs all bot actions to a file"""
 
     print(text)
-    with open('bot_log.txt', 'a') as log_file:
+    with open('logs/bot_log.txt', 'a') as log_file:
         log_file.write(f'\n[{datetime.datetime.now()}] {text}')
         log_file.close()
