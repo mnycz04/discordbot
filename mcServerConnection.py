8785354d44b571f0297e874e4852e1ad3ceddc1e
mnycz <mnycz04@gmail.com>
2021-01-12 14:33:51 -0500

    Added passive server checking

--- mcServerConnection.py
+++ mcServerConnection.py
@@ -3,24 +3,17 @@
 import socket
 
 
-class Connection:
+def ping_server( ip, port):
     """
-    Class that connects to mc server
+    Pings a server to see if its running
+    :param str ip: Host names IP
+    :param int port: Port to connect to Host
+    :return: Returns the connection, or if connection is refused, False
     """
-
-    def __init__(self, is_connected=False):
-        self.is_connected = is_connected
-
-    def ping_server(self, ip, port):
-        """
-        Pings a server to see if its running
-        :param str ip: Host names IP
-        :param int port: Port to connect to Host
-        :return: Returns the connection, or if connection is refused, False
-        """
-
-        try:
-            socket.create_connection((ip, port))
-            self.is_connected = True
-        except TimeoutError:
-            self.is_connected = False
+    try:
+        socket.create_connection((ip, port), 5)
+        return True
+    except TimeoutError:
+        return False
+    except socket.timeout:
+        return False
